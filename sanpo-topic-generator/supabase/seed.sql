9edb28e2c3d53c778f2b337ea15c55fb7ac2197b
carlykusa <shota.lovenattsu@gmail.com>
2025-05-10 21:33:02 +0900

    weather_typesテーブルのテストデータインサート

---
+++ sanpo-topic-generator/supabase/seed.sql
@@ -0,0 +1,5 @@
+-- weather_types
+INSERT INTO weather_types (id, name) VALUES
+('cc7a0a0d-f3e1-0b59-9f8d-7674ff94ed54','晴れ'),
+('0f742b2e-e147-ba50-bb44-a42bd083b64e','曇り'),
+('b47687ca-3bda-2e2f-e1a8-135d9f3d77c6','雨');
